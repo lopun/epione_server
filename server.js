e699454d0e6396745efefb243b80503f288520c3
lopun <ujin43255252@gmail.com>
2019-01-05 17:08:18 +0900

    Login function modified & connected to client

--- server.js
+++ server.js
@@ -1,27 +1,29 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-const {createConnection} = require('typeorm');
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const { createConnection } = require("typeorm");
 
-const defaultConnectionOptions = require('./src/ormConfig.js');
+const defaultConnectionOptions = require("./src/ormConfig.js");
 
-app.use(express.static(__dirname + '/public'));
-app.set('views', __dirname + '/views');
-app.engine('html', require('ejs').renderFile);
+app.use(cors());
+app.use(express.static(__dirname + "/public"));
+app.set("views", __dirname + "/views");
+app.engine("html", require("ejs").renderFile);
 
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 createConnection(defaultConnectionOptions).then(connection => {
-  console.log('DB Connected to port 3306');
+  console.log("DB Connected to port 3306");
 
-  const User = connection.getRepository('User');
+  const User = connection.getRepository("User");
 
-  require('./src/routes.js')(app, User);
+  require("./src/routes.js")(app, User);
 
-  app.listen('8000', () => {
-    console.log('server running at port 8000');
+  app.listen("8000", () => {
+    console.log("server running at port 8000");
   });
 });
--- src/routes.js
+++ src/routes.js
@@ -1,43 +1,55 @@
 module.exports = function(app, User) {
-  app.get('/', function(req, res) {
-    res.render('index.html');
+  app.get("/", function(req, res) {
+    res.render("index.html");
   });
 
-  app.get('/login', function(req, res) {
-    res.render('login.html');
+  app.get("/login", function(req, res) {
+    res.render("login.html");
   });
 
-  app.post('/login', async (req, res) => {
+  app.post("/login", async (req, res) => {
     console.log(req.body);
     let resultJson = req.body;
 
-    let foundUser = await User.findOne({email: resultJson.email});
+    let foundUser = await User.findOne({ email: resultJson.email });
 
     if (foundUser) {
       if ((foundUser.password = resultJson.password)) {
         res.json({
           ok: true,
-          id: foundUser.id,
+          id: foundUser.id
         });
       }
       return;
     }
     res.json({
       ok: false,
-      id: null,
+      id: null
     });
   });
 
-  app.post('/signup', async (req, res) => {
+  app.post("/signup", async (req, res) => {
     let resultJson = req.body;
 
-    await User.save({
-      email: resultJson.email,
-      name: resultJson.name,
-      password: resultJson.password,
-      occupation: resultJson.occupation,
-    });
+    let foundUser = await User.findOne({ email: resultJson.email });
 
-    res.json('created!');
+    if (foundUser) {
+      res.json({
+        ok: false,
+        error: "You should log in instead."
+      });
+    } else {
+      await User.save({
+        email: resultJson.email,
+        name: resultJson.name,
+        password: resultJson.password,
+        occupation: resultJson.occupation
+      });
+
+      res.json({
+        ok: true,
+        error: null
+      });
+    }
   });
 };
